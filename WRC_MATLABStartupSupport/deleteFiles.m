80ac083fce68924cf09f2f13c1f93fc3c44a2248
Michael Kutzer <kutzer@usna.edu>
2024-01-18 12:05:01 -0500

    Create deleteFiles.m

---
+++ WRC_MATLABStartupSupport/deleteFiles.m
@@ -0,0 +1,21 @@
+function deleteFiles(filenames)
+% DELETEFOLDERCONTENTS deletes all files specified in a cell array.
+%   deleteFolderContents(pname)
+%
+%   Input(s)
+%       filenames - cell array containing filenames and/or foldernames
+%
+%   M. Kutzer, 18Jan2024, USNA
+
+%% Delete all files and/or folders
+for i = 1:numel(filenames)
+    if isfolder(filenames{i})
+        % Remove folder
+        rmdir(filenames{i},'s');
+    elseif isfile(filenames{i})
+        % Remove file
+        delete(filenames{i});
+    else
+        fprintf('The following file does not exist:\n\t%s\n',filenames{i});
+    end
+end
