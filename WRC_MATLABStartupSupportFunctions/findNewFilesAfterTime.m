827b13b123100cc314da47c2016f093071e25b81
Michael Kutzer <kutzer@usna.edu>
2024-01-30 16:38:31 -0500

    Update findNewFilesAfterTime.m

--- WRC_MATLABStartupSupportFunctions/findNewFilesAfterTime.m
+++ WRC_MATLABStartupSupportFunctions/findNewFilesAfterTime.m
@@ -9,13 +9,33 @@ function fnames = findNewFilesAfterTime(t0,exts,spaths)
 %   fnames = findNewFilesAfterTime(t0,exts,spaths)
 %
 %   Input(s)
-%           t0 - scalar value specifying a serial date number (e.g. t0 = now;)
-%         exts - cell array of specific extensions to search for. If
-%                unspecified, all files are returned. 
-%       spaths - cell array of search paths within the user directory
+%           t0 - scalar value specifying a serial date number 
+%                (e.g. t0 = now;)
+%         exts - cell array of specific extensions to search for. If all 
+%                extensions should be searched, use "exts = {}".  
+%       spaths - cell array of search paths within the user directory. If
+%                unspecified, the user directory is searched. 
 %
 %   Output(s)
 %       fnames - cell array containing all filenames found
+%
+%   Example(s)
+%
+%       (1) Search for *.m and *.mat files created within the "Documents" 
+%           and "Pictures" folder of the user profile after 12-noon on 
+%           Jan. 29, 2024
+%
+%           t0 = datenum('29-Jan-2024 12:00:00');
+%           exts = {'.m','.mat'};
+%           spaths = {'Documents','Pictures'};
+%           fnames = findNewFilesAfterTime(t0,exts,spaths);
+%
+%   Common search folders: 
+%       Desktop, Documents, Downloads, Pictures, Videos
+%
+%   M. Kutzer, 29Jan2024, USNA
+
+debug = false;
 
 %% Check input(s)
 narginchk(1,3);
@@ -45,9 +65,14 @@ path_b = getenv('USERPROFILE');
 %% Define search paths
 psSearchPaths = {};
 if isempty(spaths)
+    %{
     psSearchPaths{1} = sprintf([...
         '# Specify the directory path\n',...
-        '$directoryPath = "%s"\n\n'],...
+        '$directoryPath = ''%s''\n\n'],...
+        path_b);
+    %}
+    psSearchPaths{1} = sprintf([...
+        '$directoryPath = ''%s''\n'],...
         path_b);
 else
 
@@ -58,9 +83,14 @@ else
         
         % Check for valid folder
         if isfolder( tmp_path )
+            %{
             psSearchPaths{end+1} = sprintf([...
                 '# Specify the directory path\n',...
-                '$directoryPath = "%s"\n\n'],...
+                '$directoryPath = ''%s''\n\n'],...
+                tmp_path);
+            %}
+            psSearchPaths{end+1} = sprintf([...
+                '$directoryPath = ''%s''\n'],...
                 tmp_path);
         else
             fprintf('"%s" is not a valid directory\n',tmp_path);
@@ -78,16 +108,20 @@ psFilter = '';
 if ~isempty(exts)
     
     % PowerShell - Specify the file extensions to filter
+    %{
     psFilter = sprintf([...
         '# Specify the file extensions to filter\n',...
         '$fileExtensions = @(']);
-   
+    %}
+    psFilter = sprintf([...
+        '$fileExtensions = @(']);
+
     % Combine extensions
     n = numel(exts);
     for i = 1:n
 
         % Add extension
-        psFilter = sprintf('%s"%s"',psFilter,exts{i});
+        psFilter = sprintf('%s''%s''',psFilter,exts{i});
         
         % Add comma
         if i < n
@@ -97,32 +131,71 @@ if ~isempty(exts)
     end
 
     % PowerShell - End file extensions filter
-    psFilter = sprintf('%s)\n\n',psFilter);
+    psFilter = sprintf('%s)\n',psFilter);
 
 end
 
 %% Define time bounds
 % PowerShell - Define current time
+%{
 psCurrentTime = sprintf([...
     '# Calculate the current time\n',...
+    '$currentDateTime = Get-Date\n']);
+%}
+psCurrentTime = sprintf([...
     '$currentDateTime = Get-Date\n']);
 
 %% Define search
 % PowerShell - Get files using specific filters
+%{
 psSearch = sprintf([...
     '# Get files in the specified directory and its subdirectories with specified extensions modified within the last 360 seconds\n',...
     '$recentFiles = Get-ChildItem -Path $directoryPath -Recurse | Where-Object {\n',...
     '    ($_.LastWriteTime -ge $startTime) -and ($fileExtensions -contains $_.Extension)\n',...
     '}\n\n']);
+%}
+psSearch = sprintf([...
+    '$recentFiles = Get-ChildItem -Path $directoryPath -Recurse | Where-Object {',...
+    '($_.LastWriteTime -ge $startTime) -and ($fileExtensions -contains $_.Extension)',...
+    '}\n']);
 
 %% Define display
+%{
 psDisplay = sprintf([...
     '# Display the list of recent files\n',...
     'foreach ($file in $recentFiles) {\n',...
     '    Write-Output "$($file.FullName)"\n',...
+    '}\n\n']);
+%}
+psDisplay = sprintf([...
+    'foreach ($file in $recentFiles) {',...
+    'Write-Output "$($file.FullName)"',...
     '}\n']);
 
+%% Define output file
+
+% Define filename
+fnameFileList = sprintf('newFileList_%s.txt',...
+    datestr(now,'yyyymmddHHMMSS'));
+
+%{
+psOutputFile = sprintf([...
+    '# Path to the output text file\n',...
+    '$outputFilePath = ''%s''\n',...
+    '\n',...
+    '# Write file names to the text file\n',...
+    '$recentFiles | ForEach-Object {\n',...
+    '    $_.FullName | Out-File -Append -FilePath $outputFilePath\n',...
+    '}\n'],fullfile(userpath,fnameFileList));
+%}
+psOutputFile = sprintf([...
+    '$outputFilePath = ''%s''\n',...
+    '$recentFiles | ForEach-Object {',...
+    '$_.FullName | Out-File -Append -FilePath $outputFilePath',...
+    '}\n'],fullfile(userpath,fnameFileList));
+
 %% Find contents for each search directory
+fnames = {};
 for i = 1:numel(psSearchPaths)
     
     % PowerShell - Specify search directory
@@ -132,32 +205,51 @@ for i = 1:numel(psSearchPaths)
     dt = ceil( (now-t0)*1e5 );
 
     % PowerShell - Define start time
+    %{
     psStartTime = sprintf([...
         '# Time dt seconds ago\n',...
         '$startTime = $currentDateTime.AddSeconds(-%d)\n\n'],dt);
+    %}
+    psStartTime = sprintf([...
+        '$startTime = $currentDateTime.AddSeconds(-%d)\n'],dt);
 
     % Build PowerShell Command
-    psCommand = sprintf('powershell -command "%s%s%s%s%s%s"',...
+    psCommand = sprintf(repmat('%s',1,7),...
         psSearchPath,...    % (1) Search Path
         psFilter,...        % (2) Extension Filter
         psCurrentTime,...   % (3) Current Time
         psStartTime,...     % (4) Start Time
         psSearch,...        % (5) Search Files
-        psDisplay);         % (6) Display Files
+        psDisplay,...       % (6) Display Files
+        psOutputFile);      % (7) Create Output File
     
     % Display Command
-    dispBreak = repmat('-',1,60);
-    fprintf('%s\n%s\n%s\n',dispBreak,psCommand,dispBreak);
+    if debug
+        dispBreak = repmat('-',1,60);
+        fprintf('%s\n%s%s\n',dispBreak,psCommand,dispBreak);
+    end
 
+    %{
     % Send PowerShell Command
-    [a,msg] = system(psCommand)
+    %[a,msg] = system( sprintf('powershell -command "%s"',psCommand) );
+    %disp(msg);
+    %}
 
-    disp(msg);
+    % Send PowerShell Command in a single line
+    psLines = regexp(psCommand,newline,'split');
+    [~,msg] = system( sprintf('powershell -command "%s"',strjoin(psLines, ';')) );
+
+    if debug
+        disp(msg);
+    end
+        
+    psOut = regexp(msg,newline,'split');
+    fnames = [fnames,psOut];
 end
 
-fnames = msg;
-
-
+%% Remove empty cells
+tfEmpty = cellfun(@isempty,fnames);
+fnames = fnames(~tfEmpty);
 
 
 
