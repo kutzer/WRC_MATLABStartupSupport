471eed1f34bad755f1b8e8f094ef60b04488819b
Michael Kutzer <kutzer@usna.edu>
2024-02-14 09:07:13 -0500

    Update findAllFilesAndFolders.m

--- WRC_MATLABStartupSupportFunctions/findAllFilesAndFolders.m
+++ WRC_MATLABStartupSupportFunctions/findAllFilesAndFolders.m
@@ -1,17 +1,24 @@
-function [fNames,fTypes] = findAllFilesAndFolders(pName,pIgnore)
-% FINDALLFILESANDFOLDERS recursively finds all files and folders within a 
+function [fNames,fTypes] = findAllFilesAndFolders(pName,varargin)
+% FINDALLFILESANDFOLDERS recursively finds all files and folders within a
 % specified path.
 %
 %   fNames = findAllFilesAndFolders(pName)
 %
 %   fNames = findAllFilesAndFolders(pName,pIgnore)
 %
+%   fNames = findAllFilesAndFolders(pName,tfDisp)
+%
+%   fNames = findAllFilesAndFolders(pName,pIgnore,tfDisp)
+%
 %   [fNames,fTypes] = findAllFilesAndFolders(___)
 %
 %   Input(s)
 %         pName - string defining path to search
-%       pIgnore - cell array containing folder names to ignore. Note that 
-%                 '.' and '..' folder names are ignored automatically.
+%       pIgnore - [OPTIONAL] cell array containing folder names to ignore.
+%                 Note that '.' and '..' folder names are ignored
+%                 automatically.
+%        tfDisp - [OPTIONAL] binary value specifying whether progress
+%                 should be displayed. Default value is true.
 %
 %   Output(s)
 %       fNames - cell array defining all files and folders found. These
@@ -21,23 +28,40 @@ function [fNames,fTypes] = findAllFilesAndFolders(pName,pIgnore)
 %
 %   Example(s)
 %       pName = 'C:\Users\Student';
-%       pIgnore = {'AppData','apriltag-imgs','anaconda3'}
+%       pIgnore = {'AppData','apriltag-imgs','anaconda3'};
 %       [fNames,fTypes] = findAllFilesAndFolders(pName,pIgnore);
 %
 %   M. Kutzer, 01Feb2024, USNA
 
+% Updates
+%   14Feb2024 - Updated to add a disable display flag
+
 %% Check input(s)
-narginchk(1,2);
+narginchk(1,3);
 
 if ~isfolder(pName)
     error('The path "%s" is not valid.');
 end
 
-if nargin < 2
-    pIgnore = {'.','..'};
-else
-    pIgnore = [{'.','..'},pIgnore];
+tfDisp = true;
+pIgnore = {};
+pIgnoreDefault = {'.','..'};
+switch nargin
+    case 1
+        % Use default values
+    case 2
+        if iscell(varargin{1})
+            pIgnore = varargin{1};
+        else
+            tfDisp = varargin{1};
+        end
+    case 3
+        pIgnore = varargin{1};
+        tfDisp = varargin{2};
+    otherwise
+        error('Unexpected case.');
 end
+pIgnore = [pIgnoreDefault,pIgnore];
 
 %% Find all files and folders contained within the specified path
 fNames = {};
@@ -52,22 +76,24 @@ while true
         % Mark path as checked
         tf_chk(i) = false;
         
-        % Find path contents 
+        % Find path contents
         d = dir( pt_chk{i} );
         
         % User update
-        fprintf('Directory: "%s"\n',pt_chk{i});
+        %fprintf('Directory: "%s"\n',pt_chk{i});
+        displayStatus('Directory: "%s"\n',pt_chk{i},tfDisp);
         for j = 1:numel(d)
             
             % Get full file name
             fName = fullfile( d(j).folder,d(j).name );
- 
+            
             % Add directories to check list
             if d(j).isdir
                 
                 % Check for ignored folders
                 if any( matches(pIgnore,d(j).name) )
-                    fprintf('\tFolder - IGNORED: "%s"\n',d(j).name);
+                    %fprintf('\tFolder - IGNORED: "%s"\n',d(j).name);
+                    displayStatus('\tFolder - IGNORED: "%s"\n',d(j).name,tfDisp);
                     continue
                 end
                 
@@ -78,13 +104,14 @@ while true
                 % Append full path name
                 fNames{end+1} = fName;
                 % User update
-                fprintf('\tFolder -   ADDED: "%s"\n',d(j).name);
-                
+                %fprintf('\tFolder -   ADDED: "%s"\n',d(j).name);
+                displayStatus('\tFolder -   ADDED: "%s"\n',d(j).name,tfDisp);
             else
                 % Append full path name
                 fNames{end+1} = fName;
                 % User update
-                fprintf('\t  File -   ADDED: "%s"\n',d(j).name);
+                %fprintf('\t  File -   ADDED: "%s"\n',d(j).name);
+                displayStatus('\t  File -   ADDED: "%s"\n',d(j).name,tfDisp);
                 
                 % Find file type
                 [~,~,ext] = fileparts(fName);
@@ -109,5 +136,14 @@ while true
     end
     
 end
-                
-        
+
+end
+
+%% Internal function(s)
+%--------------------------------------------------------------------------
+function displayStatus(in1,in2,tfDisp)
+
+if tfDisp
+    fprintf(in1,in2);
+end
+end
