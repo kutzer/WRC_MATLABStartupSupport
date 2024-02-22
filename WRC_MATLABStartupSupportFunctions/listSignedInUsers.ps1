1d177d7714a7c9b8d0cfb904c74ad479c7e0edf2
Michael Kutzer <kutzer@usna.edu>
2024-02-22 12:00:37 -0500

    Create listSignedInUsers.ps1

---
+++ WRC_MATLABStartupSupportFunctions/listSignedInUsers.ps1
@@ -0,0 +1,17 @@
+﻿# Define the username to remove
+$target_username = "USERNAME"
+
+# Get the currently signed-in users
+$logged_in_users = quser
+
+# Output the list of signed-in usernames, excluding the target username
+$logged_in_users | ForEach-Object {
+    # Use regex to extract the username from each line of output
+    if ($_ -match '([^\s]+)\s+') {
+        $username = $Matches[1]
+        # Check if the username matches the target username
+        if ($username -ne $target_username) {
+            Write-Host $username
+        }
+    }
+}
