c4017a969065a9010f29d0aa769e88e20d4e5bc6
Michael Kutzer <kutzer@usna.edu>
2024-02-22 14:12:28 -0500

    Create getUserInfo.ps1

---
+++ WRC_MATLABStartupSupportFunctions/getUserInfo.ps1
@@ -0,0 +1,7 @@
+﻿# Get the current username
+$username = $env:USERNAME
+
+# Execute query user command to get session information
+$queryUserInfo = query user $username
+
+Write-Output $queryUserInfo
